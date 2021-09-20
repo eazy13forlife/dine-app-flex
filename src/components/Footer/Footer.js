91dc86cc8da4474659afef775b787453ae1b0d6e
Eric B <eric_baffour-addo@alumni.brown.edu>
2021-09-20 03:10:03 -0700

    finished first page css

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,22 +6,27 @@ import "./Footer.scss";
 const Footer = () => {
   return (
     <div className="Footer">
-      <DineLogo className="Footer__logo" />
-      <address className="Footer__address">
-        <h3 className="tertiary-heading-small-footer">
-          marthwaite,sedbergh
-          <br />
-          cumbria
-          <br />
-          +00 44 123 4567
-        </h3>
-      </address>
-      <div className="Footer__hours">
-        <h3 className="tertiary-heading-small-footer">
-          Open times
-          <br /> Mon-fru:09:00Am -10:00PM
-          <br /> Sat-Sun:09:00Am-11:30pm
-        </h3>
+      <div className="Footer__logo-container">
+        <DineLogo className="Footer__logo" />
+      </div>
+
+      <div className="Footer__info">
+        <address className="Footer__address">
+          <h3 className="tertiary-heading-small-footer">
+            marthwaite,sedbergh
+            <br />
+            cumbria
+            <br />
+            +00 44 123 4567
+          </h3>
+        </address>
+        <div className="Footer__hours">
+          <h3 className="tertiary-heading-small-footer">
+            Open times
+            <br /> Mon-fru:09:00Am -10:00PM
+            <br /> Sat-Sun:09:00Am-11:30pm
+          </h3>
+        </div>
       </div>
     </div>
   );
